55e567ef0b985491d9b1fbe4f433cd64c95f0941
jasonyuchen <jasonyuchen@foxmail.com>
2019-10-07 14:48:52 +0800

    poller interface

---
+++ README.md
@@ -0,0 +1,6 @@
+# ycnt
+
+A modern C++ network library in Linux.
+
+This library refers to [chenshuo's muduo](https://www.github.com/chenshuo/muduo) in depth and is mainly for studying.
+
---
+++ ycnt/net/poller/Poller.h
@@ -0,0 +1,36 @@
+//
+// Created by jason on 2019/10/6.
+//
+
+#ifndef YCNT_YCNT_NET_POLLER_POLLER_H_
+#define YCNT_YCNT_NET_POLLER_POLLER_H_
+
+#include <vector>
+
+#include <ycnt/base/Timestamp.h>
+
+namespace ycnt
+{
+
+namespace net
+{
+
+class Channel;
+class EventLoop;
+using ChannelVec = std::vector<Channel *>;
+class Poller {
+ public:
+  virtual ~Poller() = default;
+  virtual base::Timestamp poll(int timeoutMs, ChannelVec &activeChannels) = 0;
+  virtual void updateChannel(Channel *channel) = 0;
+  virtual void removeChannel(Channel *channel) = 0;
+  virtual bool hasChannel(Channel *channel) const = 0;
+  static Poller *newEpollPoller(EventLoop *);
+};
+
+} // namespace net
+
+} // namespace ycnt
+
+
+#endif //YCNT_YCNT_NET_POLLER_POLLER_H_
